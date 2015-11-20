8670adfa82c76fb87506cd617de80f16e5104842
Eamon <Chenlulei@live.com>
2015-11-20 13:43:58 +0800

    add soluton caliburn

---
+++ samples/Caliburn.Micro.HelloWinRT81/Caliburn.Micro.HelloWinRT81/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Caliburn.Micro.WinRT.Sample.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        private readonly ISettingsService settingsService;
+        private readonly ISettingsWindowManager settingsWindowManager;
+
+        public SettingsViewModel(ISettingsService settingsService, INavigationService navigationService, ISettingsWindowManager settingsWindowManager)
+            : base(navigationService)
+        {
+            this.settingsService = settingsService;
+            this.settingsWindowManager = settingsWindowManager;
+        }
+
+        public void Open()
+        {
+            settingsService.ShowSettingsUI();
+        }
+
+        public void ShowIndependent()
+        {
+            settingsWindowManager.ShowSettingsFlyout(new SampleSettingsViewModel(), "Settings", independent: true);
+        }
+    }
+}
---
+++ src/Caliburn.Micro/PlatformProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Caliburn.Micro {
+    /// <summary>
+    /// Access the current <see cref="IPlatformProvider"/>.
+    /// </summary>
+    public static class PlatformProvider {
+        private static IPlatformProvider current;
+
+        /// <summary>
+        /// Gets or sets the current <see cref="IPlatformProvider"/>.
+        /// </summary>
+        public static IPlatformProvider Current {
+            get
+            {
+                if (current == null)
+                    throw new InvalidOperationException("PlatformProvider.Current is not set, ensure Caliburn.Micro is correctly initialized");
+                return current;
+            }
+            set { current = value; }
+        }
+    }
+}
